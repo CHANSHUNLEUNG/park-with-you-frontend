99c5a9a08301dafb820c9e7ac32fdb6bd2164a28
Veronica Sham <shamve@oocl.com>
2020-04-25 22:48:27 +0800

    [Veronica] add functions to the login/Sign out  button

--- src/ParkingLotHome/ParkingLotHomeHeader/ParkingLotHomeHeader.js
+++ src/ParkingLotHome/ParkingLotHomeHeader/ParkingLotHomeHeader.js
@@ -3,7 +3,16 @@ import logo from "../images/ParkWithYouLogo.png";
 import cat from "../images/CAT.JPG";
 import "./ParkingLotHomeHeader.css";
 import { UserOutlined, LogoutOutlined } from "@ant-design/icons";
-import { Row, Col, Layout, Button } from "antd";
+import {
+  Row,
+  Col,
+  Layout,
+  Button,
+  Popconfirm,
+  message,
+  Modal,
+  Input,
+} from "antd";
 
 const { Header } = Layout;
 
@@ -11,8 +20,68 @@ export default class ParkingLotHomeHeader extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.confirmAction = this.confirmAction.bind(this);
+    this.handleConfirm = this.handleConfirm.bind(this);
+    this.showModal = this.showModal.bind(this);
+
+    this.state = {
+      isLoggedIn: true,
+      loading: false,
+      visible: false,
+    };
   }
+
+  confirmAction() {
+    if (this.state.isLoggedIn === true) {
+      this.onLogout();
+    }
+    if (this.state.isLoggedIn === false) {
+      this.onLogin();
+    }
+  }
+
+  onLogin() {
+    this.showModal();
+    message.info("Signed in");
+    this.setState({
+      isLoggedIn: true,
+    });
+  }
+
+  onLogout() {
+    message.info("Logged Out.");
+    this.setState({
+      isLoggedIn: false,
+    });
+  }
+
+  showModal() {
+    this.setState({
+      visible: true,
+    });
+  }
+
+  handleConfirm() {
+    var confirmMessage = "";
+    if (this.state.isLoggedIn === true) {
+      confirmMessage = "Confirm to Logout? ";
+    }
+    if (this.state.isLoggedIn === false) {
+      confirmMessage = "Login now ?";
+    }
+    return confirmMessage;
+  }
+
+  handleOk = () => {
+    this.setState({ loading: true });
+    setTimeout(() => {
+      this.setState({ loading: false, visible: false });
+    }, 3000);
+  };
+
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
 
   render() {
     return (
@@ -43,13 +112,47 @@ export default class ParkingLotHomeHeader extends Component {
                     </Button>
                     <br />
                     <br />
-                    <Button
-                      shape="round"
-                      icon={<LogoutOutlined />}
-                      size="medium"
+                    <Popconfirm
+                      placement="bottom"
+                      title={this.handleConfirm}
+                      onConfirm={this.confirmAction}
+                      okText="Yes"
+                      cancelText="No"
                     >
-                      Sign Out
-                    </Button>
+                      <Button
+                        shape="round"
+                        icon={<LogoutOutlined />}
+                        size="medium"
+                      >
+                        {this.state.isLoggedIn ? "Sign out" : "Log in"}
+                      </Button>
+                    </Popconfirm>
+                    <Modal
+                      visible={this.state.visible}
+                      title="Login"
+                      onOk={this.handleOk}
+                      onCancel={this.handleCancel}
+                      footer={[
+                        <Button key="back" onClick={this.handleCancel}>
+                          Return
+                        </Button>,
+                        <Button
+                          key="submit"
+                          type="primary"
+                          loading={this.state.loading}
+                          onClick={this.handleOk}
+                        >
+                          Submit
+                        </Button>,
+                      ]}
+                    >
+                      <Input
+                        size="large"
+                        placeholder="Username"
+                        prefix={<UserOutlined />}
+                      />
+                      <Input.Password size="large" placeholder="Password" />
+                    </Modal>
                   </div>
                 </Col>
                 <Col className="gutter-row" span={15}>
