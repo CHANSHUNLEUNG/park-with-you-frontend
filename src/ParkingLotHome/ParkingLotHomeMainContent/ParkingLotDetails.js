497941eff8236ab8a0681c8a02e9169c7af1e619
Terrence Wong <wongte@oocl.com>
2020-04-28 22:37:43 +0800

    [Terrence] - add nav button

--- src/ParkingLotHome/ParkingLotHomeMainContent/ParkingLotDetails.js
+++ src/ParkingLotHome/ParkingLotHomeMainContent/ParkingLotDetails.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Card, Typography } from 'antd'
+import { Card, Typography, Row, Col, Button } from 'antd'
+import { SendOutlined } from "@ant-design/icons";
 
 export default class ParkingLotDetails extends Component {
   static propTypes = {
     parkingLot: PropTypes.object
+  }
+
+  constructor(props) {
+    super(props)
+    this.onNavigate = this.onNavigate.bind(this);
+  
+    this.state = {
+       
+    }
+  }
+  
+  onNavigate() {
+    const link = encodeURI(`https://www.google.com/maps/dir/?api=1&destination=${this.props.parkingLot.address}`);
+    window.open(link, '_blank');
+    
   }
 
   render() {
@@ -13,9 +29,16 @@ export default class ParkingLotDetails extends Component {
       <div>
         <Typography.Title style={{textAlign: "center"}}>{parkingLot.name}</Typography.Title>
         <Card>
-          <p>Address: {parkingLot.address}</p>
-          <p>Price per hour: {parkingLot.unitPrice}</p>
-          <p>Current Available Place: {parkingLot.availableCount}</p>
+          <Row>
+            <Col span={20}>
+              <p>Address: {parkingLot.address}</p>
+              <p>Price per hour: {parkingLot.unitPrice}</p>
+              <p>Current Available Place: {parkingLot.availableCount}</p>
+            </Col>
+            <Col span={4}>
+              <Button onClick={this.onNavigate} type="primary" shape="circle" icon={<SendOutlined />} size="large" />
+            </Col>
+          </Row>
         </Card>
       </div>
     );
